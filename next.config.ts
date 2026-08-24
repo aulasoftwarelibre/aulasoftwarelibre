240dde27036924f96f2dae4f47581ec267bfa088
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-08-24 11:54:05 +0200

    feat: basepath added onto next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    output: 'export'
+  output: 'export',
+  basePath: '/aulasoftwarelibre/wip'
 };
 
 export default nextConfig;
