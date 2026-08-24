0995c48a86ea64fbe558e6b0e7174b0321c2fa3b
Darío Martínez Kostyuk <dariomartk@gmail.com>
2026-08-24 12:50:31 +0200

    feat: changed header into tailwind

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -1,12 +1,18 @@
 "use client";
-import styles from "./header.module.css";
+import Image from "next/image";
 
 export default function Header() {
-    return(
-        <div className={styles.header}>
-            <div className={styles.middle_zone}>
-                <img src="logo-cuadrado_white-alpha.svg" alt="logo aula de software libre" className={styles.image}/>
-            </div>
-        </div>
-    )
+  return (
+    <div className="w-full pt-[3vw] bg-linear-to-r from-[#560f2a] via-[#60102f] via-70% to-[#540f29]">
+      <div className="w-4/5 justify-self-center">
+        <Image
+          src="/logo-cuadrado_white-alpha.svg"
+          alt="logo aula de software libre"
+          width={200}
+          height={80}
+          className="py-[5vw]"
+        />
+      </div>
+    </div>
+  );
 }
